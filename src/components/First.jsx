94361603acb56fb51c902a18fe9e1564805db301
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-10 23:33:32 +0100

    new commits

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,104 +1,104 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const First = () => {
   return (
-    <div className='bg-blue-700 h-96'>
-        <div className='flex flex-col pb-0'>
+    <div className="bg-blue-700 h-96">
+      <div className="flex flex-col pb-0">
         <Image
-              src="/1.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/1.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/2.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/2.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/3.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/3.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/4.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/4.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/5.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/5.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/6.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/6.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/7.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/7.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/8.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/8.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/9.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/9.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/10.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/10.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/11.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/11.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/12.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
+          src="/12.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
         <Image
-              src="/13.jpeg"
-              alt="grants"
-              width={435}
-              height={295}
-              className="object-center object-cover p-5"
-            />
-        </div>
+          src="/13.jpeg"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default First
+export default First;
