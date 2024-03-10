9c42d2624bc389843db8585b771ec1945f997380
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-10 23:27:54 +0100

    new pictures added

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -14,28 +14,84 @@ const First = () => {
               className="object-center object-cover p-5"
             />
         <Image
-              src="/1.jpeg"
+              src="/2.jpeg"
               alt="grants"
               width={435}
               height={295}
               className="object-center object-cover p-5"
             />
         <Image
-              src="/1.jpeg"
+              src="/3.jpeg"
               alt="grants"
               width={435}
               height={295}
               className="object-center object-cover p-5"
             />
         <Image
-              src="/1.jpeg"
+              src="/4.jpeg"
               alt="grants"
               width={435}
               height={295}
               className="object-center object-cover p-5"
             />
         <Image
-              src="/1.jpeg"
+              src="/5.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/6.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/7.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/8.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/9.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/10.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/11.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/12.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/13.jpeg"
               alt="grants"
               width={435}
               height={295}
