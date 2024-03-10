ead1ed5f57ef0efed1c602f4137795556789f16f
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-10 21:57:21 +0100

    new pictures added

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,11 +1,46 @@
+import Image from 'next/image'
 import React from 'react'
 
 const First = () => {
   return (
     <div className='bg-blue-700 h-80'>
         <h4 className='text-4xl pt-10 pl-16 text-white'>Our Services</h4>
-        <div>
-            
+        <div className='flex flex-col'>
+        <Image
+              src="/1.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/1.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/1.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/1.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
+        <Image
+              src="/1.jpeg"
+              alt="grants"
+              width={435}
+              height={295}
+              className="object-center object-cover p-5"
+            />
         </div>
     </div>
   )
