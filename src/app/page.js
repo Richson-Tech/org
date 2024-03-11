cf3c36f00a42e907d3be06fbabc5b061297859d4
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-11 23:46:18 +0100

    new commits

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Do from "@/components/Do";
 import First from "@/components/First";
 
 export default function Home() {
   return (
-<First/>
-    );
+    <main>
+      <First />
+      <Do />
+    </main>
+  );
 }
--- src/components/Do.jsx
+++ src/components/Do.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineStock } from "react-icons/ai";
 
 const Do = () => {
   return (
-    <div className=''>
-
+    <div className="bg-white h-max w-4/5">
+      <div className="flex flex-col mb-5">
+        <div>
+          <AiOutlineStock className="text-blue-600" size="2rem" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Do
+export default Do;
