eac9e2e5b35bc98aa446668db574ee7fe789c1a8
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-12 00:21:30 +0100

    new commits

--- src/components/Num.jsx
+++ src/components/Num.jsx
@@ -1,48 +1,10 @@
-// "use client";
-// import React from "react";
-// import { AiOutlineStock } from "react-icons/ai";
-// import { useState, useEffect } from "react";
-// import numeral from "numeral";
-
-
-
-// const Num = () => {
-//   const [count, setCount] = useState(0);
-//   const targetNumber = 20;
-
-//   useEffect(() => {
-//     const incrementNumber = () => {
-//       if (count < targetNumber) {
-//         // Increment the count by a certain amount (adjust as needed)
-//         setCount((prevCount) => prevCount + 20); // Adjust the increment as needed
-//       }
-//     };
-
-//     const intervalId = setInterval(incrementNumber, 100); // Adjust the interval as needed
-
-//     // Cleanup function to clear the interval when the component unmounts
-//     return () => clearInterval(intervalId);
-//   }, [count]);
-
-//   return (
-//     <div className="bg-white h-max w-full">
-//       <div className="flex flex-col mb-5">
-//         <div>
-//           <AiOutlineStock className="text-blue-600" size="2rem" />
-//           <h1>{numeral(count).format("0,0")}</h1>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Num;
-
 "use client"
 import React, { useState, useEffect } from 'react';
 import numeral from 'numeral';
+import { AiOutlineStock } from "react-icons/ai";
+import { RiTeamFill } from "react-icons/ri";
 
-const NumberLoadingComponent = () => {
+const Num = () => {
   const [currentNumber, setCurrentNumber] = useState(0);
 
   useEffect(() => {
@@ -54,18 +16,25 @@ const NumberLoadingComponent = () => {
         // Clear the interval when the number reaches 20
         clearInterval(interval);
       }
-    }, 100); // Set the interval to 1000 milliseconds (1 second)
+    }, 100); // Set the interval to 100 milliseconds
 
     // Cleanup function to clear the interval when the component is unmounted
     return () => clearInterval(interval);
   }, [currentNumber]); // Re-run the effect when the currentNumber changes
 
   return (
-    <div>
-      <p>Loading Numbers:</p>
-      <p>{numeral(currentNumber).format('0,0')}</p>
-    </div>
+<div className="bg-white h-max w-full">
+       <div className="flex flex-col mb-5">
+         <div>
+           <AiOutlineStock className="text-blue-600" size="2rem" />
+           <h1>{numeral(currentNumber).format('0,0')}</h1>
+         </div>
+         <div>
+         <RiTeamFill className="text-blue-600" size="2rem" />
+         </div>
+       </div>
+     </div>
   );
 };
 
-export default NumberLoadingComponent;
+export default Num;
