2e0fa7abbe4ddcffc4939eb7c74da9bc1cfa7d84
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-11 03:21:52 +0100

    new commits

---
+++ src/components/Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className="h-12 bg-blue-700 text-white px-4 flex items-center justify-center text-center text-sm border-2 md:text-base cursor-pointer">
+      Free delivery for all orders from $150. Order Now!
+    </div>
+  )
+}
+
+export default Top
