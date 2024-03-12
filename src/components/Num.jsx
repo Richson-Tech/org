aafd47ba5da9025d3810d5fed66b11ef5d80b8c5
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-12 13:26:54 +0100

    new commits

--- src/components/Num.jsx
+++ src/components/Num.jsx
@@ -1,11 +1,10 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import numeral from 'numeral';
+"use client";
+import React, { useState, useEffect } from "react";
+import numeral from "numeral";
 import { AiOutlineStock } from "react-icons/ai";
-import { RiTeamFill,RiSettings2Fill } from "react-icons/ri";
+import { RiTeamFill, RiSettings2Fill } from "react-icons/ri";
 import { MdPeople } from "react-icons/md";
-import Image from 'next/image';
-
+import Image from "next/image";
 
 const Num = () => {
   const [currentNumber, setCurrentNumber] = useState(0);
@@ -74,31 +73,39 @@ const Num = () => {
   }, [lastNumber]); // Re-run the effect when the lastNumber changes
 
   return (
-<div className="bg-slate-100 h-max pb-96 w-full">
-       <div className="bg-white h-max w-4/5 ml-10 flex flex-col mb-5 pb-20">
-         <div className='pt-10 pl-10 pb-5'>
-           <AiOutlineStock className="text-blue-600" size="2rem" />
-           <h1 className='text-3xl font-bold pt-2 pb-1'>{numeral(currentNumber).format('0,0')}</h1>
-           <h6 className='text-xl'>Years Of Experience</h6>
-         </div>
-         <div className='pt-10 pl-10 pb-5'> 
-         <RiTeamFill className="text-blue-600" size="2rem" />
-         <h1 className='text-3xl font-bold pt-2 pb-1'>{numeral(rightNumber).format('0,0')}</h1>
-         <h6 className='text-xl'>Team Members</h6>
-         </div>
-         <div className='pt-10 pl-10 pb-5'>
-         <RiSettings2Fill className="text-blue-600" size="2rem" />
-         <h1 className='text-3xl font-bold pt-2 pb-1'>{numeral(leftNumber).format('0,0')}K</h1>
-         <h6 className='text-xl'>Hours Of Maintenance</h6>
-         </div>
-         <div className='pt-10 pl-10 pb-5'>
-         <MdPeople className="text-blue-600" size="2rem" />
-         <h1 className='text-3xl font-bold pt-2 pb-1'>{numeral(lastNumber).format('0,0')}K</h1>
-         <h6 className='text-xl'>Happy Client</h6>
-         </div>
-       </div>
-       <div className='pt-20'>
-        <p className='pl-5'>Current of: November 14, 2024</p>
+    <div className="bg-slate-100 h-max pb-96 w-full">
+      <div className="bg-white h-max w-4/5 ml-10 flex flex-col mb-5 pb-20">
+        <div className="pt-10 pl-10 pb-5">
+          <AiOutlineStock className="text-blue-600" size="2rem" />
+          <h1 className="text-3xl font-bold pt-2 pb-1">
+            {numeral(currentNumber).format("0,0")}
+          </h1>
+          <h6 className="text-xl">Years Of Experience</h6>
+        </div>
+        <div className="pt-10 pl-10 pb-5">
+          <RiTeamFill className="text-blue-600" size="2rem" />
+          <h1 className="text-3xl font-bold pt-2 pb-1">
+            {numeral(rightNumber).format("0,0")}
+          </h1>
+          <h6 className="text-xl">Team Members</h6>
+        </div>
+        <div className="pt-10 pl-10 pb-5">
+          <RiSettings2Fill className="text-blue-600" size="2rem" />
+          <h1 className="text-3xl font-bold pt-2 pb-1">
+            {numeral(leftNumber).format("0,0")}K
+          </h1>
+          <h6 className="text-xl">Hours Of Maintenance</h6>
+        </div>
+        <div className="pt-10 pl-10 pb-5">
+          <MdPeople className="text-blue-600" size="2rem" />
+          <h1 className="text-3xl font-bold pt-2 pb-1">
+            {numeral(lastNumber).format("0,0")}K
+          </h1>
+          <h6 className="text-xl">Happy Client</h6>
+        </div>
+      </div>
+      <div className="pt-20">
+        <p className="pl-5">Current of: November 14, 2024</p>
         <Image
           src="/logo.png"
           alt="Logo"
@@ -106,9 +113,36 @@ const Num = () => {
           height={195}
           className="object-center object-cover p-5"
         />
-
-       </div>
-     </div>
+        <p className="pt-2 text-center p-2">
+          The Community Services Block Grant (CSBG) is a federally funded block
+          grant in the Office of Community Services, Administration for Children
+          and Families, United States Department of Health and Human Services
+          that provides funds to states, territories, and tribes to administer
+          to support services that alleviate the causes and conditions of
+          poverty in under resourced communities. Tribes, territories, and over
+          1,000 local Community Action Agencies provide CSBG funded services and
+          activities including housing, nutrition, utility, and transportation
+          assistance; employment, education, and other income and asset building
+          services; crisis and emergency services; and community asset building
+          initiatives, among other things. Over 9 million individuals are served
+          by CSBG-funded programs annually.
+        </p>
+        <Image
+          src="/17.png"
+          alt="Logo"
+          width={135}
+          height={195}
+          className="object-center object-cover p-5"
+        />
+        <Image
+          src="/18.png"
+          alt="Logo"
+          width={135}
+          height={195}
+          className="object-center object-cover p-5"
+        />
+      </div>
+    </div>
   );
 };
 
