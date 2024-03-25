e504e9d943c504d4110ab6e483b82d4c20dc044b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-25 13:19:56 +0100

    chaport added

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,8 +1,103 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Services page</div>
+    <div className="bg-blue-700 h-max w-full pb-32">
+    <div className="flex flex-col pb-0">
+      <Image
+        src="/1.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/2.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/3.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/4.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/5.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/6.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/7.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/8.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/9.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/10.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/11.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/12.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+      <Image
+        src="/13.jpeg"
+        alt="grants"
+        width={435}
+        height={295}
+        className="object-center object-cover p-5"
+      />
+    </div>
+  </div>
   )
 }
 
