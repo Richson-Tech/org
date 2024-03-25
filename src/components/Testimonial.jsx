6b3361ee600a8e21f708f0fede091412c04fc413
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-25 17:26:16 +0100

    chaport added

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,6 +6,18 @@ const Testimonial = () => {
       <h1 className="text-4xl text-center text-[#111518] pl-5 font-[roboto] font-bold pb-5 pt-16">
         Testimonial
       </h1>
+      <div className="flex flex-col">
+        <div className="bg-white rounded">
+        <Image
+          src="/alice.webp"
+          alt="grants"
+          width={435}
+          height={295}
+          className="object-center object-cover p-5"
+        />
+
+        </div>
+      </div>
     </div>
   );
 };
