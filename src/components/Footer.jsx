132882f0a4b800766ee8502b2ac099b356b8113a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 05:38:20 +0100

    new commits added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <div className="bg-[#11BF97] flex flex-col p-12 text-white">
+        <h6 className="text-black font-bold text-xl text-center">Contact Us</h6>
+        <p className="pt-5 pb-8 text-center text-xl font-[roboto]">
+          phone: (781)2691505
+        </p>
+        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
+          Need help or have a question?
+        </p>
+        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
+          Contact us at:
+        </p>
+        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
+          support@c-s-b-g.org
+        </p>
+      </div>
+      <div className="bg-white">
+        <p className="text-[#757575] font-[inter] text-center pt-3 pb-3">
+          All content © 2006-2023 Community Service Block Grant, and its
+          representatives. All rights reserved
+        </p>
+      </div> 
+    </div>
+  )
+}
+
+export default Footer
