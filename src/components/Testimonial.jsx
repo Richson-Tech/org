ee6508b4aa4fa24697c0b8d18c5f1486b7cc5050
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 05:33:37 +0100

    new commits added

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -7,7 +7,7 @@ const Testimonial = () => {
       <h1 className="text-4xl text-center text-[#111518] pl-5 font-[roboto] font-bold pb-5 pt-16">
         Testimonial
       </h1>
-      <div className="flex flex-col">
+      <div className="flex flex-col space-y-10">
         <div className="bg-slate-50 w-67 rounded">
           <div className="pt-5">
             <Image
@@ -19,7 +19,37 @@ const Testimonial = () => {
             />
           </div>
           <p className="text-xl pt-2 text-center font-semibold">Alice Lee</p>
-          <p className="pt-2 text-center p-2 text-[#757575]">
+          <p className="pt-2 text-center p-2 pb-10 text-[#757575]">
+            Tested and Trusted
+          </p>
+        </div>
+        <div className="bg-slate-50 w-67 rounded">
+          <div className="pt-5">
+            <Image
+              src="/alice.webp"
+              alt="grants"
+              width={155}
+              height={195}
+              className="object-center object-cover pl-20 ml-20"
+            />
+          </div>
+          <p className="text-xl pt-2 text-center font-semibold">Alice Lee</p>
+          <p className="pt-2 text-center p-2 pb-10 text-[#757575]">
+            Tested and Trusted
+          </p>
+        </div>
+        <div className="bg-slate-50 w-67 rounded">
+          <div className="pt-5">
+            <Image
+              src="/alice.webp"
+              alt="grants"
+              width={155}
+              height={195}
+              className="object-center object-cover pl-20 ml-20"
+            />
+          </div>
+          <p className="text-xl pt-2 text-center font-semibold">Alice Lee</p>
+          <p className="pt-2 text-center p-2 pb-10 text-[#757575]">
             Tested and Trusted
           </p>
         </div>
