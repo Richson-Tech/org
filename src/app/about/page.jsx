38abd1afe707acb9854575e2221b33b9f86ab112
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 06:05:09 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,6 +11,23 @@ const page = () => {
         communities. Our social service and community development programs work
         in a variety of ways to improve the lives of many.
       </p>
+      <div className="flex flex-col">
+     <div className="bg-slate-50 w-67 rounded">
+          <div className="pt-5">
+            <Image
+              src="/alice.webp"
+              alt="grants"
+              width={155}
+              height={195}
+              className="object-center object-cover pl-20 ml-20"
+            />
+          </div>
+          <p className="text-xl pt-2 text-center font-semibold">Alice Lee</p>
+          <p className="pt-2 text-center p-2 pb-10 text-[#757575]">
+            Tested and Trusted
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
