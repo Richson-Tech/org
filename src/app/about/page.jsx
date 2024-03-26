1619c234ad72fc8d4bfddb098fe7bbf026426f90
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 05:46:05 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <div className="bg-blue-700 h-max w-full pb-32">
       <p>
-      The Office of Community Services (OCS) partners with states, tribes, territories, and non-profit and community-based organizations to reduce the causes and consequences of poverty, increase opportunity and economic security of individuals and families, and revitalize communities. Our social service and community development programs work in a variety of ways to improve the lives of many.
+        The Office of Community Services (OCS) partners with states, tribes,
+        territories, and non-profit and community-based organizations to reduce
+        the causes and consequences of poverty, increase opportunity and
+        economic security of individuals and families, and revitalize
+        communities. Our social service and community development programs work
+        in a variety of ways to improve the lives of many.
       </p>
-
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
