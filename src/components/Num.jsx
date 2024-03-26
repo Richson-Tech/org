2e6cd34ee2767b0bdfa8cb82582f35f3421b2eff
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 05:38:54 +0100

    new commits added

--- src/components/Num.jsx
+++ src/components/Num.jsx
@@ -6,6 +6,7 @@ import { RiTeamFill, RiSettings2Fill } from "react-icons/ri";
 import { MdPeople } from "react-icons/md";
 import Image from "next/image";
 import Maps from "./Maps";
+import Footer from "./Footer";
 
 const Num = () => {
   const [currentNumber, setCurrentNumber] = useState(0);
@@ -442,27 +443,7 @@ const Num = () => {
         </div>
       </div>
       <Maps />
-      <div className="bg-[#11BF97] flex flex-col p-12 text-white">
-        <h6 className="text-black font-bold text-xl text-center">Contact Us</h6>
-        <p className="pt-5 pb-8 text-center text-xl font-[roboto]">
-          phone: (781)2691505
-        </p>
-        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
-          Need help or have a question?
-        </p>
-        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
-          Contact us at:
-        </p>
-        <p className="pt-2 pb-3 text-center text-xl font-[roboto]">
-          support@c-s-b-g.org
-        </p>
-      </div>
-      <div className="bg-white">
-        <p className="text-[#757575] font-[inter] text-center pt-3 pb-3">
-          All content © 2006-2023 Community Service Block Grant, and its
-          representatives. All rights reserved
-        </p>
-      </div>
+      <Footer/>
     </div>
   );
 };
