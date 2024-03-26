00e2ecc253e917ffc1a98139c86daaafe827fcda
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 05:59:23 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="bg-blue-700 h-max w-full pb-32">
-      <p>
+      <p className="text-center p-2 pt-10 text-white">
         The Office of Community Services (OCS) partners with states, tribes,
         territories, and non-profit and community-based organizations to reduce
         the causes and consequences of poverty, increase opportunity and
