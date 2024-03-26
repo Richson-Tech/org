d0b3502e71c712cc8ee430962f959e46ece64aa3
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-26 14:00:21 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="bg-blue-700 h-max w-full pb-32">
+    <div className="bg-blue-700 h-96 w-full pb-32">
       <p className="text-center p-2 pt-10 text-white">
         The Office of Community Services (OCS) partners with states, tribes,
         territories, and non-profit and community-based organizations to reduce
@@ -12,20 +12,21 @@ const page = () => {
         communities. Our social service and community development programs work
         in a variety of ways to improve the lives of many.
       </p>
-      <div className="flex flex-col">
-     <div className="bg-slate-50 w-30 h-36 rounded">
-          <div className="pt-5">
-            <Image
-              src="/kyle.webp"
-              alt="grants"
-              width={155}
-              height={195}
-              className="object-center object-cover pl-20 ml-20"
-            />
-          </div>
-          <p className="text-xl pt-2 text-center font-semibold">Kyle Estrada</p>
-        </div>
-      </div>
+      <div className="flex flex-col justify-center items-center">
+  <div className="bg-slate-50 w-30 h-36 rounded flex justify-center items-center pt-52">
+    <div>
+      <Image
+        src="/kyle.webp"
+        alt="grants"
+        width={500}
+        height={310}
+        className="object-cover"
+      />
+    </div>
+  </div>
+  <p className="text-xl pt-2 text-center font-semibold">Kyle Estrada</p>
+</div>
+
     </div>
   );
 };
