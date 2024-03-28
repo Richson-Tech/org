bc232c4d2e8a825748e32f446fe7ff6dc7336930
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-28 21:52:44 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -59,8 +59,8 @@ const page = () => {
       <div>
         <h1 className="text-3xl text-center pl-5 font-[roboto] font-bold pb-5 pt-16">CSBG State Plan Toolkit</h1>
         <p className="text-[#757575] pt-2 text-center p-2">Publication Date: June 12, 2023</p>
-        <p>Current as of: June 14, 2023</p>
-        <p>
+        <p className="text-[#757575] pt-2 text-center p-2">Current as of: June 14, 2023</p>
+        <p className="text-[#757575] pt-2 text-center p-2">
           Table of Contents The CSBG State Plan Toolkit is a resource for CSBG
           grant recipients that provides links to communications, forms and
           guidelines, webinars, and demos created by the Office of Community
