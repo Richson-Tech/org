13ee74e445b9bf5eaf786505baa1d91fd546b8f4
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-28 21:54:06 +0100

    new commits added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -51,22 +51,32 @@ const page = () => {
         </div>
       </div>
       <div>
-        <h1 className="text-3xl text-center pl-5 font-[roboto] font-bold pb-5 pt-16">We Are Here To Help</h1>
+        <h1 className="text-3xl text-center pl-5 font-[roboto] font-bold pb-5 pt-16">
+          We Are Here To Help
+        </h1>
         <div>
-          <p className="pt-2 text-center p-2">304 North Cardinal St. Dorchester Center</p>
+          <p className="pt-2 text-center p-2">
+            304 North Cardinal St. Dorchester Center
+          </p>
         </div>
       </div>
       <div>
-        <h1 className="text-3xl text-center pl-5 font-[roboto] font-bold pb-5 pt-16">CSBG State Plan Toolkit</h1>
-        <p className="text-[#757575] pt-2 text-center p-2">Publication Date: June 12, 2023</p>
-        <p className="text-[#757575] pt-2 text-center p-2">Current as of: June 14, 2023</p>
-        
+        <h1 className="text-3xl text-center pl-5 font-[roboto] font-bold pb-5 pt-16">
+          CSBG State Plan Toolkit
+        </h1>
         <p className="text-[#757575] pt-2 text-center p-2">
-           The CSBG State Plan Toolkit is a resource for CSBG
-          grant recipients that provides links to communications, forms and
-          guidelines, webinars, and demos created by the Office of Community
-          Services to aid CSBG grant recipients in the creation and submission
-          of the CSBG State Plan.
+          Publication Date: June 12, 2023
+        </p>
+        <p className="text-[#757575] pt-2 text-center p-2">
+          Current as of: June 14, 2023
+        </p>
+        <p className="text-[#757575] pt-2 text-center p-2">Table of Contents</p>
+        <p className="text-[#757575] pt-2 text-center p-2">
+          The CSBG State Plan Toolkit is a resource for CSBG grant recipients
+          that provides links to communications, forms and guidelines, webinars,
+          and demos created by the Office of Community Services to aid CSBG
+          grant recipients in the creation and submission of the CSBG State
+          Plan.
         </p>
       </div>
     </div>
