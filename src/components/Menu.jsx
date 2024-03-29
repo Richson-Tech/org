503d7d53b34718fdb5826443cff2f99b3a60fbb1
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-29 20:15:53 +0100

    new commits added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,22 +19,30 @@ const Menu = () => {
   return (
     <div>
       {open ? (
-        <MdOutlineClose className="text-white" size="2rem" onClick={() => setOpen(false)}  />
+        <MdOutlineClose
+          className="text-white"
+          size="2rem"
+          onClick={() => setOpen(false)}
+        />
       ) : (
-        <CgMenu className="text-white" size="2rem" onClick={() => setOpen(true)} />
+        <CgMenu
+          className="text-white"
+          size="2rem"
+          onClick={() => setOpen(true)}
+        />
       )}
 
       {open && (
         <div className="bg-white text-black absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2] z-10">
-         <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={135}
-          height={195}
-          className="object-center object-cover p-5"
-        />
-    </Link>
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={135}
+              height={195}
+              className="object-center object-cover p-5"
+            />
+          </Link>
           {links.map((item) => (
             <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
               {item.title}
