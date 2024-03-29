e625e3e0614ac78e39bdad4b507ce50c5d062f67
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-29 07:04:16 +0100

    new commits added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,16 +7,61 @@ const page = () => {
     <div className="bg-blue-700 h-32 w-full pb-32">
       <div className="flex flex-col pb-0">
         <h2 className="text-4xl font-serif text-white text-center pt-10 font-normal">
-          Our Services
+          Contact Us
         </h2>
         <Maps />
       </div>
-      <div className="flex flex-col">
-        <IoLocationSharp className="text-blue-700 pl-2" size="2.5rem" />
-        <h5 className="text-2xl text-center pt-10 font-bold">
-          Physical Address
-        </h5>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
+          <IoLocationSharp
+            className="text-blue-700 text-center"
+            size="2.5rem"
+          />
+          <h5 className="text-2xl pt-5 pb-3 font-bold">Physical Address</h5>
+        </div>
+        <p className="text-lg text-[#757575] pb-1 font-[inter] text-center pl-5 pr-3">
+          330 C Street, S.W.
+        </p>
         <p className="text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3">
+          Washington, D.C. 20201
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
+          <IoLocationSharp
+            className="text-blue-700 text-center"
+            size="2.5rem"
+          />
+          <h5 className="text-2xl pt-5 pb-3 font-bold">Phone Numbers</h5>
+        </div>
+        <p className="text-lg text-[#757575] pb-1 font-[inter] text-center pl-5 pr-3">
+          7812691505
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
+          <IoLocationSharp
+            className="text-blue-700 text-center"
+            size="2.5rem"
+          />
+          <h5 className="text-2xl pt-5 pb-3 font-bold">Physical Address</h5>
+        </div>
+        <p className="text-lg text-[#757575] pb-1 font-[inter] text-center pl-5 pr-3">
+          330 C Street, S.W.
+        </p>
+        <p className="text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3">
+          Washington, D.C. 20201
+        </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center">
+          <IoLocationSharp
+            className="text-blue-700 text-center"
+            size="2.5rem"
+          />
+          <h5 className="text-2xl pt-5 pb-3 font-bold">Physical Address</h5>
+        </div>
+        <p className="text-lg text-[#757575] pb-1 font-[inter] text-center pl-5 pr-3">
           330 C Street, S.W.
         </p>
         <p className="text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3">
