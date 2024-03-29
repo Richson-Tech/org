e49061c112f25bc100e80d5d5d2599b462cd4082
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-29 05:59:44 +0100

    new commits added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Contact page</div>
+    <div className='bg-blue-700 h-32 w-full pb-32'>
+      <div>
+      <h2 className="text-4xl font-serif text-white text-center pt-10 font-normal">
+          Our Services
+        </h2>
+      </div>
+
+    </div>
   )
 }
 
