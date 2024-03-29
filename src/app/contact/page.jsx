7a4d004a378289c67190645616c9ae1753ae4507
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-29 06:56:36 +0100

    new commits added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,25 +1,30 @@
-import Maps from '@/components/Maps'
-import React from 'react'
+import Maps from "@/components/Maps";
+import React from "react";
 import { IoLocationSharp } from "react-icons/io5";
 
 const page = () => {
   return (
-    <div className='bg-blue-700 h-32 w-full pb-32'>
-      <div className='flex flex-col pb-0'>
-      <h2 className="text-4xl font-serif text-white text-center pt-10 font-normal">
+    <div className="bg-blue-700 h-32 w-full pb-32">
+      <div className="flex flex-col pb-0">
+        <h2 className="text-4xl font-serif text-white text-center pt-10 font-normal">
           Our Services
         </h2>
-        <Maps/>
+        <Maps />
       </div>
-      <div className='flex flex-col'>
-      <IoLocationSharp className="text-blue-700 pl-2" size="2.5rem" />
-      <h5 className='text-2xl text-center pt-10 font-bold'>Physical Address</h5>
-      <p className='text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3'>330 C Street, S.W.</p>
-      <p className='text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3'>Washington, D.C. 20201</p>
+      <div className="flex flex-col">
+        <IoLocationSharp className="text-blue-700 pl-2" size="2.5rem" />
+        <h5 className="text-2xl text-center pt-10 font-bold">
+          Physical Address
+        </h5>
+        <p className="text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3">
+          330 C Street, S.W.
+        </p>
+        <p className="text-lg text-[#757575] pb-5 font-[inter] text-center pl-5 pr-3">
+          Washington, D.C. 20201
+        </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default page 
+export default page;
