2968134b542a0790be9991da9fee0083b72604f6
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-03-29 06:01:53 +0100

    new commits added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,14 @@
+import Maps from '@/components/Maps'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='bg-blue-700 h-32 w-full pb-32'>
-      <div>
+      <div className='flex flex-col pb-0'>
       <h2 className="text-4xl font-serif text-white text-center pt-10 font-normal">
           Our Services
         </h2>
+        <Maps/>
       </div>
 
     </div>
